c6db13c7593b08de0652342e394087498b83b274
deroffal <alex_roffde@hotmail.com>
2018-04-03 12:27:08 +0200

    Day10 - part 2

--- src/main/java/fr/deroffal/aoc/Day10.java
+++ src/main/java/fr/deroffal/aoc/Day10.java
@@ -1,6 +1,7 @@
 package fr.deroffal.aoc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,23 +11,32 @@ import fr.deroffal.aoc.util.Utils;
 
 public class Day10 {
 
-	public static void main(String[] args) {
-		final List<Integer> lengths = Utils.readLineToIntegerList("day10.txt", ",");
-		final List<Integer> numbers = IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList());
+	private static final List<Integer> SEQUENCE_END = Arrays.asList(17, 31, 73, 47, 23);
 
-		int part1 = getPart1(numbers, lengths);
+	private static int position;
+	private static int skipSize;
+
+	public static void main(String[] args) {
+		final List<Integer> lengthSequence = Utils.readLineToIntegerList("day10.txt", ",");
+
+		int part1 = part1(IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList()), lengthSequence);
 		System.out.println("Partie 1 : " + part1);
+
+		final String input = Utils.readLine("day10.txt");
+		System.out.println("Partie 2 : " + doKnotHash(input, IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList())));
 	}
 
-	static int getPart1(final List<Integer> numbers, final List<Integer> lengths) {
-		doOneStep(numbers, lengths);
+	static int part1(final List<Integer> numbers, final List<Integer> lengthSequence) {
+		position = 0;
+		skipSize = 0;
+		doSingleRound(numbers, lengthSequence);
 		return numbers.get(0) * numbers.get(1);
 	}
 
-	private static void doOneStep(final List<Integer> numbers, final List<Integer> lengths) {
-		int position = 0;
-		for (int skipSize = 0; skipSize < lengths.size(); skipSize++) {
-			position = reverse(numbers, position, lengths.get(skipSize), skipSize);
+	private static void doSingleRound(final List<Integer> numbers, final List<Integer> lengthSequence) {
+		for (final Integer length : lengthSequence) {
+			position = reverse(numbers, position, length, skipSize);
+			skipSize++;
 		}
 	}
 
@@ -40,6 +50,46 @@ public class Day10 {
 		IntStream.range(0, sublist.size()).forEach(i -> numbers.set((position + i) % inputSize, sublist.get(i)));
 
 		return (position + length + skipSize) % inputSize;
+	}
+
+	static String doKnotHash(final String input, final List<Integer> numbers) {
+		//initialisation
+		position = 0;
+		skipSize = 0;
+		final List<Integer> lengthSequence = getLength(input);
+
+		//sparse hash
+		doSparseHash(numbers, lengthSequence, 64);
+
+		final StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 16; i++) {
+			final int start = i * 16;
+			int xor = numbers.get(start);
+			for (int j = 1; j < 16; j++) {
+				final Integer integer = numbers.get(start + j);
+				xor = xor ^ integer;
+			}
+			String hexa = Integer.toHexString(xor);
+
+			String padded = "00".substring(hexa.length()) + hexa;
+			sb.append(padded);
+		}
+
+
+		//dense hash
+		return sb.toString();
+	}
+
+	static List<Integer> getLength(final String input) {
+		//toAscii
+		final List<Integer> lengthSequence = input.chars().boxed().collect(Collectors.toList());
+		//ajouter le suffixe
+		lengthSequence.addAll(SEQUENCE_END);
+		return lengthSequence;
+	}
+
+	static void doSparseHash(final List<Integer> numbers, final List<Integer> lengthSequence, final int numberOfRounds) {
+		IntStream.range(0, numberOfRounds).forEach(round -> doSingleRound(numbers, lengthSequence));
 	}
 
 }
--- src/test/java/fr/deroffal/aoc/Day10Test.java
+++ src/test/java/fr/deroffal/aoc/Day10Test.java
@@ -17,7 +17,7 @@ class Day10Test {
 		final List<Integer> lengths = Arrays.asList(3, 4, 1, 5);
 		final List<Integer> numbers = IntStream.rangeClosed(0, 4).boxed().collect(Collectors.toList());
 
-		assertEquals(12, Day10.getPart1(numbers, lengths));
+		assertEquals(12, Day10.part1(numbers, lengths));
 	}
 
 	/**
@@ -59,4 +59,27 @@ class Day10Test {
 		assertEquals(1, nextPos);
 	}
 
+	/**
+	 * Conversion ascii + ajout de la constante à la fin
+	 */
+	@Test
+	void conversionAscii() {
+		assertEquals(Arrays.asList(49, 44, 50, 44, 51, 17, 31, 73, 47, 23), Day10.getLength("1,2,3"));
+	}
+
+	@Test
+	void doKnotHash_for_EmptyString() {
+		final List<Integer> numbers = IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList());
+		assertEquals("a2582a3a0e66e6e86e3812dcb672a272", Day10.doKnotHash("", numbers));
+	}
+
+	@Test
+	void doKnotHash_for_otherExample() {
+		List<Integer> numbers = IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList());
+		assertEquals("33efeb34ea91902bb2f59c9920caa6cd", Day10.doKnotHash("AoC 2017", numbers));
+		numbers = IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList());
+		assertEquals("3efbe78a8d82f29979031a4aa0b16a9d", Day10.doKnotHash("1,2,3", numbers));
+		numbers = IntStream.rangeClosed(0, 255).boxed().collect(Collectors.toList());
+		assertEquals("63960835bcdc130f0b66d7ff4f6a5a8e", Day10.doKnotHash("1,2,4", numbers));
+	}
 }
